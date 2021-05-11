773750ea5279f44085203f028539b6ae97f16503
jadeven <deveneyjoseph@gmail.com>
2021-05-10 18:14:51 -1000

    Delete http-file-server.js

--- http-file-server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const fs = require('fs')
-
-const server = http.createServer(function (req, res) {
-    const stream = fs.createReadStream(process.argv[3])
-    stream.pipe(res);
-})
-
-server.listen(process.argv[2])
